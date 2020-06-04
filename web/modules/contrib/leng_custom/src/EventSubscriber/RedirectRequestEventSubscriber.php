dc4356a00dc67c69ce9da62a20a2e9056ce23421
daveduya011 <dave.duya@gmail.com>
2020-06-04 23:44:33 +0800

    Commit

--- web/modules/contrib/leng_custom/src/EventSubscriber/RedirectRequestEventSubscriber.php
+++ web/modules/contrib/leng_custom/src/EventSubscriber/RedirectRequestEventSubscriber.php
@@ -17,11 +17,11 @@ class RedirectRequestEventSubscriber implements EventSubscriberInterface {
     $request_uri = urldecode(\Drupal::request()->getRequestUri());  
     $current_user = \Drupal::currentUser()->id();
 
-    // if (preg_match('/\{user\}/', $request_uri)) {
-    //   $request_uri = preg_replace('/\{user\}/', $current_user, $request_uri);
-    //   $response = new RedirectResponse($request_uri, 301);
-    //   $response->send();
-    // }
+    if (preg_match('/\{user\}/', $request_uri)) {
+      $request_uri = preg_replace('/\{user\}/', $current_user, $request_uri);
+      $response = new RedirectResponse($request_uri, 301);
+      $response->send();
+    }
     if ($request_uri == "/user/".$current_user) {
       $request_uri = sprintf("/user/%s/orders", $current_user);
       $response = new RedirectResponse($request_uri, 301);
